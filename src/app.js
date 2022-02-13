d677c79191a3e12c6d03735aa8a860b33c233f48
AliDemircix <alidemircix@gmail.com>
2022-02-13 01:33:55 +0100

    api key seperated

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 // import { getWeatherInfo } from './getWeatherInfo';
+import { API_KEY } from './constants.js';
 const locationName = document.querySelector('.location-timezone');
 const temperature = document.querySelector('.temperature-degree');
 const description = document.querySelector('.temperature-description');
 const iconImg = document.querySelector('#wicon');
 let lon;
 let lat;
-const API_KEY = '277c6d1cede61ce80d475b5bd9f80f67';
 
 window.addEventListener('load', async () => {
   if (navigator.geolocation) {
---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const API_KEY = '277c6d1cede61ce80d475b5bd9f80f67';
